4b1dfd06b8b18b8526893eb8562ebe4c1641244e
Chase Adam <chaseadam17@users.noreply.github.com>
2021-10-21 11:21:15 -0700

    Add notion to footer

--- components/BlankFooter.jsx
+++ components/BlankFooter.jsx
@@ -32,7 +32,7 @@ const BlankFooter = ({ social }) => {
 
 BlankFooter.defaultProps = {
   index: "index",
-  social: [{"href":"https://discord.gg/HZM2jcHdEU","name":"Discord"},{"href":"https://twitter.com/blankfdn","name":"Twitter"},{"href":"https://medium.com/@blankfdn","name":"Medium"}]
+  social: [{"href":"https://discord.gg/HZM2jcHdEU","name":"Discord"},{"href":"https://twitter.com/blankfdn","name":"Twitter"},{"href":"https://medium.com/@blankfdn","name":"Medium"},{"https://www.notion.so/blankfoundation/Blank-Home-f3fd5330df0d4933aa41e363719c2625","name":"Notion"}]
 }
 
 export default BlankFooter;
