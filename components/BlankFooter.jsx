17422bee738b7920fa93da085795fdf06dbb48fc
Jared Cosulich <jared.cosulich@gmail.com>
2021-11-10 12:34:17 -0500

    updated notion link

--- components/BlankFooter.jsx
+++ components/BlankFooter.jsx
@@ -32,7 +32,7 @@ const BlankFooter = ({ index, social }) => {
 
 BlankFooter.defaultProps = {
   index: "index",
-  social: [{"href":"https://discord.gg/HZM2jcHdEU","name":"Discord"},{"href":"https://twitter.com/blankfdn","name":"Twitter"},{"href":"https://medium.com/@blankfdn","name":"Medium"},{"href":"https://www.notion.so/blankfoundation/Blank-Home-f3fd5330df0d4933aa41e363719c2625","name":"Notion"}]
+  social: [{"href":"https://discord.gg/HZM2jcHdEU","name":"Discord"},{"href":"https://twitter.com/blankfdn","name":"Twitter"},{"href":"https://medium.com/@blankfdn","name":"Medium"},{"href":"https://blankfoundation.notion.site/Blank-641ad836df9c4918b1bd09ce196a6dce","name":"Notion"}]
 }
 
 export default BlankFooter;
