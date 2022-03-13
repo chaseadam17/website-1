101de02b846180524b0e710f51c16b5424558e86
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-12 21:24:22 -0500

    combined art section

--- components/EvolutionCollection.jsx
+++ components/EvolutionCollection.jsx
@@ -1,29 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   NextLink,
   TWButton,
-  SupabaseImage
+  SupabaseImage,
+  CombineArt
 } from '.'
 import supabaseClient from '../lib/supabaseClient';
+
+const ADMIN_WALLET = '0xfa23B55345c7237b7eEE52Db975E8a72b840BC1A';
 
 const EvolutionCollection = ({ collection, provider }) => {
   const [file, setFile] = useState(null);
   const [art, setArt] = useState(collection?.art || []);
+  const [wallet, setWallet] = useState(null);
 
   const imageUri = (id) => `${collection.title}/${id}.png`
 
   const submit = async () => {
-    if (!file) return;
-
-    const wallet = await provider.getSigner();
-    const address = await wallet.getAddress();
+    if (!file || !wallet) return;
 
     const insert = async () => {
       const { body, error } = await supabaseClient
         .from('art')
         .insert({
           collection_id: collection.id,
-          wallet: address
+          wallet: wallet
         })
 
       if (error) console.log("INSERT ERROR", error)
@@ -48,8 +49,18 @@ const EvolutionCollection = ({ collection, provider }) => {
     const artItem = await insert();
     await storage(artItem.id);
   
-    setArt([artItem, ...art])
+    setArt([...art, artItem])
   }
+
+  useEffect(() => {
+    const getWallet = async () => {
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      setWallet(address);
+    }
+
+    getWallet();
+  }, [provider])
 
   if (!collection) {
     return (
@@ -61,7 +72,7 @@ const EvolutionCollection = ({ collection, provider }) => {
     <div>
       <h1 className='text-sm'>
         <NextLink
-          href='/member'
+          href='/members'
         >
           <a className='text-blue-600 underline'>Evolution Challenge</a> 
         </NextLink>
@@ -69,26 +80,42 @@ const EvolutionCollection = ({ collection, provider }) => {
         {collection.title}
       </h1>
 
-      <div className='py-12'>
-        <input
-          type='file'
-          className='mr-3'
-          onChange={(e) => setFile(e.target.files[0])}
+      <div className='py-6'>
+        <CombineArt
+          art={art} 
+          collection={collection}
         />
-        <TWButton
-          onClick={submit}
-        >
-          Submit
-        </TWButton>
       </div>
 
-      <div className='py-12 flex'>
+      <hr className='my-6' />
+      
+      {wallet &&
+        <div className='py-6'>
+          <p>Upload An Image</p>
+          <p className='text-xs py-3'>Your image should be 2000px x 2000px.</p>
+          <input
+            type='file'
+            className='mr-3'
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <TWButton
+            onClick={submit}
+          >
+            Submit
+          </TWButton>
+        </div>
+      }
+
+      <div className='py-6 flex'>
         {art.map(
           (artItem, index) => (
-            <div key={`art-${art.id}`} className='mr-12'>
+            <div key={`art-${artItem.id}`} className='mr-6'>
               <SupabaseImage
                 collection={collection}
                 item={artItem}
+                index={index + 1}
+                dim={120}
+                ownerAdmin={wallet === artItem.wallet || wallet === ADMIN_WALLET}
               />
             </div>
           )
