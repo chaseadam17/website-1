9681bcd80481117d422e6e7de789e6592b66ffa5
Jared Cosulich <jared.cosulich@gmail.com>
2022-04-25 13:54:55 -0400

    locking is working

--- components/MemberNfts.jsx
+++ components/MemberNfts.jsx
@@ -4,6 +4,7 @@ import { BlankArt } from '../contracts';
 
 const MemberNfts = ({ provider, wallet }) => {
   const [tokenIds, setTokenIds] = useState([]);
+  const [lockedMap, setLockedMap] = useState({})
 
   useEffect(() => {
     const loadTokens = async () => {
@@ -25,11 +26,37 @@ const MemberNfts = ({ provider, wallet }) => {
           delete tokenIdsMap[transfer.args.tokenId]
         }
       }
-      setTokenIds(Object.keys(tokenIdsMap))
+
+      const _tokenIds = Object.keys(tokenIdsMap)
+      setTokenIds(_tokenIds)
+
+      const _lockedMap = {}
+      for (const _tokenId of _tokenIds) {
+        const locked = await contract.tokenURILocked(_tokenId)
+        _lockedMap[_tokenId] = locked.toString() === "1";
+      }
+
+      setLockedMap(_lockedMap)
     }
 
     loadTokens();
   }, [provider, wallet])
+
+  const lockNft = async (tokenId) => {
+    if (!confirm('Are you sure you want to lock this Blank NFT?')) return;
+
+    const contract =  new ethers.Contract(BlankArt.address, BlankArt.abi, provider);
+    const signer = provider.getSigner();
+    const connectedSigner = contract.connect(signer)
+
+    const tx = await connectedSigner.lockTokenURI(tokenId);
+    await tx.wait();
+
+    setLockedMap({
+      ...lockedMap,
+      [tokenId]: true
+    })
+  }
 
   return (
     <div className='py-12'>
@@ -37,7 +64,7 @@ const MemberNfts = ({ provider, wallet }) => {
         Your Blank NFTs
       </div>
 
-      <div className='py-12 text-center text-sm'>
+      <div className='py-12 px-24 text-center text-sm'>
         Between now and June 1st you can lock your Blank NFT to prevent it from evolving.
         <br/>
         <br/>
@@ -45,26 +72,31 @@ const MemberNfts = ({ provider, wallet }) => {
         <br/>
         <br/>
         More information about the evolution can be found in Discord.
+        <br/>
+        <br/>
+        Click "Lock NFT" on any NFT below to lock it. You will need to sign the transaction and can reject it to cancel.
       </div>
 
-      <div className='flex flex-wrap gap-3'>
+      <div className='pl-24 pr-6 flex flex-wrap gap-3'>
         {tokenIds.map(
           (tokenId) => (
             <div 
               key={`token-${tokenId}`}
-              className='border w-48 h-48 flex flex-col justify-between'
+              className={`border w-48 h-48 flex flex-col justify-between ${lockedMap[tokenId] ? 'bg-gray-300' : ''}`}
             >
               <div className='p-3 text-lg text-center font-bold'>
                 {tokenId}
               </div>
               <div className='text-center'>
-                Not Locked
+                {lockedMap[tokenId] ? <>Locked</> : <>Not Locked</>}
               </div>
-              <div className='p-3 border-t text-xs flex justify-around'>
-                <div className='cursor-pointer'>
-                  &#128274; Lock NFT
+              {!lockedMap[tokenId] ? (
+                <div className='p-3 border-t text-xs flex justify-around'>
+                  <div className='cursor-pointer' onClick={() => lockNft(tokenId)}>
+                    &#128274; Lock NFT
+                  </div>
                 </div>
-              </div>
+              ) : <div className='h-12'></div>}
             </div>
           )
         )}
